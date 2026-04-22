8ebbcb7e7568b524dcd1a39fa738009d36b21f29
DeadRuins <wipoppi666@gmail.com>
2026-04-22 18:01:30 +0900

    touch up on things

--- codegen.py
+++ codegen.py
@@ -12,6 +12,7 @@ def generate_zscript(frame_label, normal_frame, blink_frame, openmouth_frame, op
 
     count = calculate_tics(seconds)
     lines = []
+    zscript_func = "CB_DialogueSkipPrevent;"
 
     if(len(frame_label) != 4):
         raise ValueError("frame_label needs to be 4 letter long!")
@@ -19,21 +20,26 @@ def generate_zscript(frame_label, normal_frame, blink_frame, openmouth_frame, op
 
     # Lead-in
     lines.append(f'TNT1 A 0 CB_SpeakDialogue(#DialogueNumber1, #DialogueNumber2, "Villy", "Very_Angry" );')
-    lines.append(f'{frame_label} {normal_frame} 9 CB_DialogueSkipPrevent;')
-    lines.append(f'{frame_label} {normal_frame} 1 CB_DialogueSkipPrevent;')
 
-    # Main Loop
-    for i in range(10, count):
+    #lines.append(f'{frame_label} {normal_frame} 9 {zscript_func}')
+    #lines.append(f'{frame_label} {normal_frame} 1 {zscript_func}')
+
+    # Main Loop, if something complex animation such as lipsync gets neccearly.
+    for i in range(0, count):
+        if(i > 10):
+            zscript_func = f'A_JumpIfInTargetInventory("SkipDialogue", 1, "{goto_label}");'
+        else:
+            zscript_func = "CB_DialogueSkipPrevent;"
+
         if i in mouth_tics:
             IsSpeaking = not IsSpeaking
-            print(IsSpeaking)
 
         if IsSpeaking:
-            current_frame = blink_frame if (i % 60 < 3) else normal_frame
+            current_frame = blink_frame_func(i, openmouth_frame, openmouth_eyeclosed_frame) if (i % 8 < 4) else blink_frame_func(i, normal_frame, blink_frame)
         else:
-            current_frame = openmouth_frame if (i % 8 < 4) else normal_frame
+            current_frame = blink_frame_func(i, normal_frame, blink_frame)
 
-        command = f'{frame_label} {current_frame} 1 A_JumpIfInTargetInventory("SkipDialogue", 1, "{goto_label}");'
+        command = f'{frame_label} {current_frame} 1 {zscript_func}'
         lines.append(command)
 
     lines.append(f"\n{goto_label}:")
@@ -53,3 +59,8 @@ def parse_mouth_list(input_string):
     except ValueError:
         raise ValueError("Mouth Movements must be a list of numbers separated by commas!")
 
+def blink_frame_func(i, normal_frame, blink_frame):
+    if (i % 60 < 3) and (i > 10):
+        return blink_frame
+    else:
+        return normal_frame
